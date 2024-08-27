97237b484b5326047236d04b5d1b2b71ba047f2c
Alenta <mef082024@gmail.com>
2024-08-27 10:13:28 +0200

    Counter

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Initialize jScript");
+const introSection = document.getElementById("introSection");
+console.dir(introSection);
+
+//Counter Logic
+const counterElement = document.getElementById("counter");
