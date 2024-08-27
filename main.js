fd3ca1b1b75e8fe8b2a24fd4729a381e6e60b359
Alenta <mef082024@gmail.com>
2024-08-27 11:06:53 +0200

    Finished counter

--- main.js
+++ main.js
@@ -3,4 +3,33 @@ const introSection = document.getElementById("introSection");
 console.dir(introSection);
 
 //Counter Logic
-const counterElement = document.getElementById("counter");
+//const counterElement = document.getElementById("counter");
+const counterElement = document.querySelector("#counter");
+
+const display = counterElement.querySelector("span");
+console.log(display);
+console.dir(counterElement);
+
+const plusButton = counterElement.querySelector(".plus");
+const minusButton = counterElement.querySelector(".minus");
+const resetButton = counterElement.querySelector(".reset");
+
+function handleIncrement() {
+  const currentValue = Number(display.textContent);
+  const newValue = currentValue + 1;
+  display.textContent = newValue;
+}
+function handleDecrement() {
+  const currentValue = Number(display.textContent);
+  const newValue = currentValue - 1;
+
+  display.textContent = newValue;
+}
+
+function reset() {
+  display.textContent = 0;
+}
+
+plusButton.addEventListener("click", handleIncrement);
+minusButton.addEventListener("click", handleDecrement);
+resetButton.addEventListener("click", reset);
